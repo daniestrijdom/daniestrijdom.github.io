84b3dc0319a26f1d4d518944934986c24627a2e4
daniestrijdom <daansteraan@gmail.com>
2016-07-23 18:02:38 +0200

    fine... REAL initial commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/daansteraan/pen/gPEmzr.
+
+ Let me be frank; I hate this. It's messy and ugly, but we all have to start somewhere; 
+
+There will definitely be a re-do of this in future once I am more comfortable with this stuff. I need to read and write quite a bit more code first. 
---
+++ js/index.js
@@ -0,0 +1,19 @@
+$("#g").on("click", function() { 
+  $("#general").slideToggle()
+})
+
+$("#w").on("click", function() { 
+  $("#work").slideToggle()
+})
+
+$("#e").on("click", function() { 
+  $("#education").slideToggle()
+})
+
+$("#s").on("click", function() { 
+  $("#skillset").slideToggle()
+})
+
+$("#c").on("click", function() { 
+  $("#contact").slideToggle()
+})
