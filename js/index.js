4b0dfe8b8ddbb061233e6172f6e11edf1a6a09eb
daniestrijdom <daansteraan@gmail.com>
2016-07-23 19:02:01 +0200

    a few more fixes

--- js/index.js
+++ js/index.js
@@ -1,22 +1,25 @@
+$(document).ready(function() {
+
 $("#g").on("click", function() {
   $("#general").slideToggle()
-})
+});
 
 $("#w").on("click", function() {
   $("#work").slideToggle()
-})
+});
 
 $("#e").on("click", function() {
   $("#education").slideToggle()
-})
+});
 
 $("#s").on("click", function() {
   $("#skillset").slideToggle()
-})
+});
 
 $("#c").on("click", function() {
   $("#contact").slideToggle()
-})
+});
 
 
 console.log("working?")
+});
